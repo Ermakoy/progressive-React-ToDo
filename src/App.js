a7fcd9197edb3ea4595fa7728b8babdaed490613
Andrey <own3d.rus@gmail.com>
2018-02-10 19:27:53 +0300

    ✅ Item addition added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,41 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      items : []
+    };
+    this.addItem = this.addItem.bind(this);
+  }
+  addItem(e) {
+    e.preventDefault();
+    const itemArray = this.state.items;
+
+    if (this._inputValue !== ''){
+      itemArray.unshift({
+        text: this._inputValue,
+        date: Date.now()
+      });
+
+      this.setState({
+        items: itemArray
+      });
+
+      this._inputValue = '';
+      console.log(itemArray);
+
+    }
+  }
   render() {
     return (
       <div className="todolistMain">
         <div className="header">
-          <form action="">
-            <input type="text" placeholder='Enter the task'/>
+          <form onSubmit={this.addItem}>
+            <input type="text"
+                   ref={(a) => this._inputValue = a}
+                   placeholder='Enter the task'/>
             <button type="submit">Add</button>
           </form>
         </div>
